826cc55a5aec853b04632dc1d1c7f140d86bd8f4
bhlzlx <bhlzlx@gmail.com>
2018-08-13 21:32:39 +0800

    添加头文件

---
+++ VkApiLoader/vkApi.h
@@ -0,0 +1,21 @@
+#pragma once
+
+#ifdef _WIN32
+	#define VK_NO_PROTOTYPES
+	#define VK_USE_PLATFORM_WIN32_KHR
+#endif
+#include "vulkan/vulkan.h"
+
+#define VULKAN_EXPORT_API( api ) static PFN_##api api;
+#define VULKAN_GLOBAL_API( api ) static PFN_##api api;
+#define VULKAN_INSTANCE_API( api ) static PFN_##api api;
+#define VULKAN_DEVICE_API( api ) static PFN_##api api;
+
+namespace clannad
+{
+	namespace vulkan
+	{
+#include "inl/functions.h"
+	}
+
+}
